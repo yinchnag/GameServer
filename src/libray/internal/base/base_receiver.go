b5005bf74c1a966f68b19560ee0d685f367dad80
careing <835635194@qq.com>
2025-05-08 23:06:35 +0800

    记录反射函数数据，增加回调函数逻辑

--- src/libray/internal/base/base_receiver.go
+++ src/libray/internal/base/base_receiver.go
@@ -7,7 +7,14 @@ import (
 	"wgame_server/libray/interfaces"
 )
 
-var ReceiverFunc []string = []string{}
+// 反射函数信息 -- 运行时数据，不能存盘
+// 该结构体用于存储函数的反射信息
+type FuncInfo struct {
+	funcName   string         // 函数名称
+	method     reflect.Value  // 函数反射对象
+	inputArgs  []reflect.Kind // 输入参数类型
+	outputArgs []reflect.Kind // 输出参数类型
+}
 
 var (
 	_ interfaces.IRceiver = &BaseReceiver{}
@@ -17,26 +24,73 @@ var (
 // 模块对象基础类
 // 继承(组合)该类的对象需主动调用Init函数
 type BaseReceiver struct {
-	invald map[string]reflect.Value // 所有反射获得的函数
-	name   string                   // 对象名称
+	invokers map[string]*FuncInfo // 所有反射获得的函数
+	name     string               // 对象名称
 }
 
 // 初始化对象
 // 在任何情况下创建出对象后该函数一定是第一个调用，否则会出现painc
 func (that *BaseReceiver) Init(val any) {
-	that.invald = make(map[string]reflect.Value)
-	that.reflectFunc(val)
+	that.invokers = make(map[string]*FuncInfo)
+	that.SetInvokerAll(val)
 }
 
-func (that *BaseReceiver) reflectFunc(val any) {
-	// valValue := reflect.ValueOf(val)
-	valType := reflect.TypeOf(val)
-	that.name = valType.Name()
-	for i := 0; i < valType.NumMethod(); i++ {
-		method := valType.Method(i)
-		fmt.Printf("bind method: %s\n", method.Name)
-		that.invald[method.Name] = method.Func
+// 设置对象中所有函数的反射
+func (that *BaseReceiver) SetInvokerAll(val any) {
+	instVal := reflect.ValueOf(val)
+	instType := instVal.Type()
+	that.name = instType.Name()
+	methodNum := instType.NumMethod()
+	for i := 0; i < methodNum; i++ {
+		name := instType.Method(i).Name
+		method := instVal.Method(i)
+		if method.CanInterface() {
+			kind := method.Kind()
+			_ = kind
+			that.SetInvoker(name, method)
+		}
 	}
+}
+
+// 判断是否为函数，如果是，则记录
+func (that *BaseReceiver) SetInvoker(funcName string, method reflect.Value) {
+	if method.Kind() != reflect.Func {
+		return
+	}
+	info := &FuncInfo{
+		funcName: funcName,
+		method:   method,
+	}
+	typeOf := method.Type()
+	for i := 0; i < typeOf.NumIn(); i++ { // 获取输入参数类型
+		info.inputArgs = append(info.inputArgs, typeOf.In(i).Kind())
+	}
+	for i := 0; i < typeOf.NumOut(); i++ { // 获取输出参数类型
+		info.outputArgs = append(info.outputArgs, typeOf.Out(i).Kind())
+	}
+	that.invokers[funcName] = info
+	fmt.Printf("bind method: %s\n", funcName)
+}
+
+func (that *BaseReceiver) Invoker(uid int64, funcName string, args ...any) (_ []reflect.Value, err error) {
+	if invoker, ok := that.invokers[funcName]; ok {
+		defer func() {
+			if err := recover(); err != nil {
+				fmt.Printf("调用函数 %s 失败, err: %v\n", funcName, err)
+				err = fmt.Errorf("调用函数 %s 失败, err: %v", funcName, err)
+			}
+		}()
+		if len(args) != len(invoker.inputArgs) {
+			return nil, fmt.Errorf("参数数量不匹配")
+		}
+		inArgs := make([]reflect.Value, 1)
+		for i, arg := range args {
+			inArgs[i] = reflect.ValueOf(arg)
+		}
+		outArgs := invoker.method.Call(inArgs)
+		return outArgs, nil
+	}
+	return nil, fmt.Errorf("没有找到函数 %s", funcName)
 }
 
 func (that *BaseReceiver) GetName() string {
@@ -47,7 +101,7 @@ func (that *BaseReceiver) HandlerEvent() {
 }
 
 func (that *BaseReceiver) Destory() {
-	that.invald = nil
+	that.invokers = nil
 }
 
 // 模块被启动时第一个调用
--- src/libray/internal/test/base_receiver_test.go
+++ src/libray/internal/test/base_receiver_test.go
@@ -1,6 +1,8 @@
 package test
 
 import (
+	"fmt"
+	"strconv"
 	"testing"
 
 	"wgame_server/libray/internal/base"
@@ -14,8 +16,19 @@ func (that *User) Init() {
 	that.BaseReceiver.Init(that)
 }
 
+func (that *User) Action(num int) string {
+	return strconv.Itoa(num)
+}
+
 func TestNewReceiver(t *testing.T) {
 	u := &User{}
 	u.Init()
+	ret, err := u.Invoker(1, "Action", 1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Printf("%v", ret)
+
 	_ = u
 }
